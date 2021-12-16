69dca709823a4fea87553329dbc8f3ef4fbad84d
AndrewPochapsky <andrew.pochapsky@gmail.com>
2021-12-16 13:05:23 -0500

    Added some documentation

--- src/msg.rs
+++ src/msg.rs
@@ -9,20 +9,26 @@ pub struct InstantiateMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
+    /// If name is not specified the sender's address will be used as the name of the primitive.
+    /// This allows each user to have easy access to a single primitive if that is all they
+    /// require.
     SetValue {
         name: Option<String>,
         value: Primitive,
     },
-    DeleteValue {
-        name: Option<String>,
-    },
+    /// If name is not specified the sender's address will be used as the name of the primitive.
+    DeleteValue { name: Option<String> },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
     Config {},
-    GetValue { address: Addr, name: Option<String> },
+    /// If name is not specified the sender's address will be used as the name of the primitive.
+    GetValue {
+        address: Addr,
+        name: Option<String>,
+    },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
